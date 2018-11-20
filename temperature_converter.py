6395b6cb8e8ad9247b0f93fabee018ceecd7330b
Divin Kayitare <kayitaredivin@gmail.com>
2018-11-20 21:11:11 +0200

    the input is now in kelvin

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,4 +1,4 @@
-temp= eval(input('enter temperature in celsius:'))
-kelvin= temp+273
-fahrenheit= 9/5*temp+32
-print('the result is {:.3f} kelvin and {:.3f} fahrenheit '.format(kelvin, fahrenheit))
+temp= eval(input('enter temperature in kelvin:'))
+celsius= temp-273
+fahrenheit= 9/5*(temp-273)+32
+print('the result is {:.3f} kelvin and {:.3f} fahrenheit '.format(celsius, fahrenheit))
